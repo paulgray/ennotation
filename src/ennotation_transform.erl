11e584612982fe558d626f5a18ce0fe8510a0e99
Michal Ptaszek <michal.ptaszek@erlang-solutions.com>
2010-08-27 17:20:20 +0200

    Implemented skeleton for transform_function_after

--- src/ennotation_transform.erl
+++ src/ennotation_transform.erl
@@ -71,208 +71,36 @@ transform_tree([{attribute, _, user_ennotation, {Args, 'after', Mod, Func}} | Re
 transform_tree([F | Rest], Tree, [], []) ->
     transform_tree(Rest, [F | Tree], [], []);
 transform_tree([{function, _, _, _, _} = F | Rest], Tree, Before, After) ->
-    NewF = transform_function(F, lists:reverse(Before), lists:reverse(After)),
+    NewF = transform_function(F, Before, After),
     transform_tree(Rest, [NewF | Tree], [], []);
 transform_tree([Element | Rest], Tree, Before, After) ->
     transform_tree(Rest, [Element | Tree], Before, After);
 transform_tree([], Tree, _, _) ->
     lists:reverse(Tree).
 
+%% Before and After lists are coming in reverse order, but 
+%% as we built it from the end it is correct
 -spec(transform_function/3 :: (tuple(), list(), list()) -> tuple()).
-transform_function({function, Line, FunName, Arity, Clauses}, Before, After) ->
-    put(function_name, FunName),
-    put(function_arity, Arity),
+transform_function(Func, Before, After) ->
+    NewFunc = transform_function_before(Func, Before),
+    transform_function_after(NewFunc, After).
 
-    NewClauses = transform_clause(Clauses, Before, After, []),
-    {function, Line, FunName, Arity, NewClauses}.
+%% FIXME: mocked function
+-spec(transform_function_before/2 :: (tuple(), list()) -> tuple()).
+transform_function_before({function, Line, Name, Artiy, Clauses} = F, 
+                          [{Args, AMod, AFunc} | Rest]) ->
+    F.
 
--spec(transform_clause/4 :: (list(), list(), list(), list()) -> list()).
-transform_clause([OrgClause | Rest], Before, After, Clauses) ->
-    BeforeClause = transform_clause_before(OrgClause, Before),
-    AfterClause = transform_clause_after(BeforeClause, After),
-    transform_clause(Rest, Before, After, [AfterClause | Clauses]);
-transform_clause([], _, _, Clauses) ->
-    lists:reverse(Clauses).
+-spec(transform_function_after/2 :: (tuple(), list()) -> tuple()).
+transform_function_after({function, Line, Name, Artity, Clauses} = F,
+                         [{Args, AMod, AFunc} | Rest]) ->
+    OrgFuncName = unique_function_name(Name, AFunc),
+    ClauseArgs = generate_args(Artity),
+    NewClause = {clause, Line, ClauseArgs, [], NewBody},
 
--spec(transform_clause_before/2 :: (tuple(), list()) -> tuple()).
-transform_clause_before(Clause, []) ->
-    Clause;
-transform_clause_before({clause, L, CArgs, Guards, Body}, Annotations0) ->
-    TgtFunM = get(module_name),
-    TgtFunN = get(function_name),
-    Arity = get(function_arity),
+    {function, Line, Name, Artity, [NewClause]}.
 
-    Annotations = prepare_annotations(Annotations0, L),
-
-    AFunName = get_unique_atom(),
-    FuncArgs = get_unique_atom(),
-    AArgs = get_unique_atom(),
-    Mod = get_unique_atom(),
-    Func = get_unique_atom(),
-    Rest = get_unique_atom(),
-    Self = get_unique_atom(),
-    NewArgs = get_unique_atom(),
-    EMod = get_unique_atom(),
-    EFunc = get_unique_atom(),
-
-    NewBody = [{match, L,
-                {var, L, AFunName},
-                {'fun', L,
-                 {clauses,
-                  [{clause, L,
-                    [{var, L, FuncArgs},
-                     {cons, L,
-                      {tuple, L,
-                       [{var, L, AArgs}, {var, L, Mod}, {var, L, Func}]},
-                      {var, L, Rest}},
-                     {var, L, Self}],
-                    [],
-                    [{'case', L,
-                      {call, L,
-                       {remote, L, {var, L, Mod}, {var, L, Func}},
-                       [{var, L, AArgs},
-                        {atom, L, TgtFunM},
-                        {atom, L, TgtFunN},
-                        {var, L, FuncArgs}]},
-                      [{clause, L,
-                        [{tuple, L, [{atom, L, proceed}, {var, L, NewArgs}]}],
-                        [],
-                        [{call, L,
-                          {var, L, Self},
-                          [{var, L, NewArgs}, {var, L, Rest}, {var, L, Self}]}]},
-                       {clause, L, 
-                        [{tuple, L, [{atom, L, skip}, {var, L, NewArgs}]}],
-                        [],
-                        [{var, L, NewArgs}]},
-                       {clause, L,
-                        [{tuple, L,
-                          [{atom, L, error},
-                           {tuple, L,
-                            [{var, L, EMod},
-                             {var, L, EFunc},
-                             {var, L, NewArgs}]}]}],
-                        [],
-                        [{call, L, 
-                          {atom, L, apply},
-                          [{var, L, EMod},
-                           {var, L, EFunc},
-                           {var, L, NewArgs}]}]}]}]},
-                   {clause, L, 
-                    [{var, L, FuncArgs}, {nil, L}, {var, L, '_'}],
-                    [],
-                    [{'if', L,
-                      [{clause, L, [],
-                        [[{op, L, '=/=',
-                           {call, L, {atom, L, length}, [{var, L, FuncArgs}]},
-                           {integer, L, Arity}}]],
-                        [{call, L,
-                          {atom, L, throw},
-                          [{tuple, L,
-                            [{atom, L, bad_annotation_result_length},
-                             {call, L,
-                              {atom, L, length},
-                              [{var, L, FuncArgs}]}]}]}]},
-                       {clause, L, [],
-                        [[{atom, L, true}]],
-                        [{call, L,
-                          {atom, L, apply},
-                          [{'fun', L,
-                            {clauses, 
-                             [{clause, L,
-                               CArgs,
-                               [],
-                               Body}]}},
-                           {var, L, FuncArgs}]}]}]}]}]}}},
-               {call, L, 
-                {var, L, AFunName},
-                [prepare_arguments_list(CArgs, L),
-                 Annotations,
-                 {var, L, AFunName}]}],
-
-    {clause, L, CArgs, Guards, NewBody}.
-
-transform_clause_after(Clause, []) ->
-    Clause;
-transform_clause_after({clause, L, CArgs, Guards, Body}, Annotations0) ->
-    TgtFunM = get(module_name),
-    TgtFunN = get(function_name),
-
-    Annotations = prepare_annotations(Annotations0, L),
-
-    OrgFunName = get_unique_atom(),
-    AFunName = get_unique_atom(),
-    FuncResult = get_unique_atom(),
-    AArgs = get_unique_atom(),
-    Mod = get_unique_atom(),
-    Func = get_unique_atom(),
-    Rest = get_unique_atom(),
-    Self = get_unique_atom(),
-    NewResult = get_unique_atom(),
-    EMod = get_unique_atom(),
-    EFunc = get_unique_atom(),
-
-    NewBody = [{match, L,
-                {var, L, OrgFunName},
-                {'fun', L, 
-                 {clauses,
-                  [{clause, L, [], [],
-                    Body}]}}},
-               {match, L, 
-                {var, L, AFunName},
-                {'fun', L,
-                 {clauses,
-                  [{clause, L,
-                    [{var, L, FuncResult},
-                     {cons, L,
-                      {tuple, L,
-                       [{var, L, AArgs}, {var, L, Mod}, {var, L, Func}]},
-                      {var, L, Rest}},
-                     {var, L, Self}],
-                    [],
-                    [{'case', L,
-                      {call, L,
-                       {remote, L, {var, L, Mod}, {var, L, Func}},
-                       [{var, L, AArgs},
-                        {atom, L, TgtFunM},
-                        {atom, L, TgtFunN},
-                        {var, L, FuncResult}]},
-                      [{clause, L,
-                        [{tuple, L, [{atom, L, proceed}, {var, L, NewResult}]}],
-                        [],
-                        [{call, L,
-                          {var, L, Self},
-                          [{var, L, NewResult}, {var, L, Rest}, {var, L, Self}]}]},
-                       {clause, L, 
-                        [{tuple, L, [{atom, L, skip}, {var, L, NewResult}]}],
-                        [],
-                        [{var, L, NewResult}]},
-                       {clause, L,
-                        [{tuple, L,
-                          [{atom, L, error},
-                           {tuple, L,
-                            [{var, L, EMod},
-                             {var, L, EFunc},
-                             {var, L, NewResult}]}]}],
-                        [],
-                        [{call, L, 
-                          {atom, L, apply},
-                          [{var, L, EMod},
-                           {var, L, EFunc},
-                           {var, L, NewResult}]}]}]}]},
-                   {clause, L, 
-                    [{var, L, FuncResult}, {nil, L}, {var, L, '_'}],
-                    [],
-                    [{var, L, FuncResult}]}]}}},
-               {call, L,
-                {var, L, AFunName},
-                [{call, L,
-                  {var, L, OrgFunName}, []},
-                 Annotations,
-                 {var, L, AFunName}]}],
-
-    {clause, L, CArgs, Guards, NewBody}.
-
--spec(prepare_arguments_list/2 :: (list(tuple()), integer()) -> tuple()).	     
+-spec(prepare_arguments_list/2 :: (list(), integer()) -> tuple()).
 prepare_arguments_list([H | R], Line) ->
     {cons, Line, H,
      prepare_arguments_list(R, Line)};
@@ -283,6 +111,11 @@ prepare_arguments_list([], Line) ->
 get_unique_atom() ->
     list_to_atom(lists:flatten(io_lib:format("~w", [now()]))).
 
+-spec(unique_function_name/2 :: (atom(), atom()) -> atom()).
+unique_function_name(OrgFun, AFun) ->
+    list_to_atom(lists:flatten(io_lib:format("~w_~w_~w", [erlang:phash2({OrgFun, AFun}), 
+                                                          OrgFun, AFun]))).
+
 -spec(prepare_annotations/2 :: (list(), integer()) -> term()).
 prepare_annotations(Annotations, Line) ->
     NewLines = [$\n || _ <- lists:seq(1, Line-1)],
@@ -291,3 +124,10 @@ prepare_annotations(Annotations, Line) ->
     {ok, [Parsed]} = erl_parse:parse_exprs(Tokens),
 
     Parsed.
+
+%% FIXME - add line number
+-spec(generate_args/1 :: (integer()) -> list()).    
+generate_args(Arity) ->
+    lists:reverse(lists:foldl(fun(N, Acc) ->
+                                      [{var, 1, list_to_atom("Arg" ++ N)} | Acc]
+                              end, [], lists:seq(1, Arity))).
